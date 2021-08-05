6e1a3f310a58f586bd98b8ec93e2342c27053ad3
Melanie Bacou <mel@mbacou.com>
2021-08-05 10:28:47 +0100

    docx

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cd "$(dirname "$(realpath "$0")")";
 rm -R ./_main.*
 rm -R ./_bookdown_files
 
-Rscript -e "bookdown::render_book('./R/', output_dir='../docs');"
+Rscript -e "bookdown::render_book('./R/', 'gitbook', output_dir='../docs');"
+Rscript -e "bookdown::render_book('./R/', 'word_document', output_dir='../docx');"
